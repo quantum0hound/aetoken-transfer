6969e6eaa51628f3688a2b9be89c0f0bc748a992
johndoe <quantum0hound@gmail.com>
2022-03-31 21:31:17 +0300

    couple of refactorings

--- quasar.conf.js
+++ quasar.conf.js
@@ -48,7 +48,7 @@ module.exports = configure(function (ctx) {
       vueRouterMode: "hash", // available values: 'hash', 'history'
 
       transpile: true,
-      //publicPath: '/',
+      publicPath: '/',
 
       // Add dependencies for transpiling with Babel (Array of string/regex)
       // (from node_modules, which are by default not transpiled).
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,15 +1,15 @@
 const routes = [
   {
     path: "/",
-    component: () => import("layouts/MainLayout.vue"),
-    children: [{ path: "", component: () => import("pages/Index.vue") }],
+    component: () => import("../layouts/MainLayout.vue"),
+    children: [{ path: "", component: () => import("../pages/Index.vue") }],
   },
 
   // Always leave this as last one,
   // but you can also remove it
   {
     path: "/:catchAll(.*)*",
-    component: () => import("pages/Error404.vue"),
+    component: () => import("../pages/Error404.vue"),
   },
 ];
 
